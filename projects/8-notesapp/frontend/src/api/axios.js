23ffc3725a88778ac49633f78111269f4bf89088
MuhammadFaraz032 <muhammadfaraz1225@gmail.com>
2026-06-14 16:10:29 +0500

    Update API URL to Railway backend

--- projects/8-notesapp/frontend/src/api/axios.js
+++ projects/8-notesapp/frontend/src/api/axios.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "https://full-stack-internship-production.up.railway.app/api",
 });
-
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
